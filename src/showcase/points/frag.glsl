c7228ac71508d11f1c4d1cd060d0df645fd6ba0d
migalooo <migalooo@yahoo.com>
2019-08-28 19:41:47 +0800

    shape function in shader

--- src/showcase/points/frag.glsl
+++ src/showcase/points/frag.glsl
@@ -2,13 +2,25 @@
 precision mediump float;
 #endif
 
-uniform vec2 u_resolution; // 画布尺寸（宽，高）
-/* uniform vec2 u_mouse;      // 鼠标位置（在屏幕上哪个像素） */
-uniform float u_time;     // 时间（加载后的秒数）
+uniform vec2 u_resolution;
+/* uniform vec2 u_mouse; */
+uniform float u_time; 
 
-void main() {
-  vec2 st = gl_FragCoord.xy/u_resolution;
-	gl_FragColor = vec4(st.x,st.y,0.0,1.0);
-  /* gl_FragColor = vec4(abs(sin(u_time)),0.0,0.0,1.0); */
+float plot(vec2 st, float pct){
+  return  smoothstep( pct-0.002, pct, st.y) - smoothstep( pct, pct+0.002, st.y);
 }
 
+void main() {
+  const float PI = 3.1415926;
+  vec2 st = gl_FragCoord.xy/u_resolution;
+
+  float y = PI*(0.1*st.x);
+
+  vec3 color = vec3(y);
+
+  float pct = plot(st,y);
+
+  color = (1.0-pct)*color+pct*vec3(0.0, 1.0, 0.0);
+
+  gl_FragColor = vec4(color,1.0);
+}
